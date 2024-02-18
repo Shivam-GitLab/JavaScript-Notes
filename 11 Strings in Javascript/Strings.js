e92e18f8ad9e5e762d01716ee426f3722faf2ffc
ShivamWiki <shivam_223033@saitm.org>
2024-02-18 17:03:49 +0530

    Source Code

--- 11 Strings in Javascript/Strings.js
+++ 11 Strings in Javascript/Strings.js
@@ -10,7 +10,7 @@ console.log(name + repoCount + 789); // Shivam Jha50789
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String("Shivam Jha");
+const gameName = new String("Shivam-Jha");
 
 console.log(gameName[0]); // S
 console.log(gameName[7]); // J
@@ -23,4 +23,25 @@ console.log(gameName.length);
 console.log(gameName.toLocaleLowerCase()); // shivam jha     Copy Send
 console.log(gameName);
 
-console.log(gameName.charAt(8)); // h index no according
+console.log(gameName.charAt(8)); // index value send -  index no according
+
+console.log(gameName.indexOf("a"));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(0, -2);
+
+console.log(anotherString);
+
+const newStringOne = "    Shivam      ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://shivamvats.me/shivam%20jha";
+console.log(url.replace("%20", "-"));
+console.log(url.includes("shivam")); // true;
+
+console.log(url.includes("shivammm")); // false;
+
+console.log(gameName.split("-"));
