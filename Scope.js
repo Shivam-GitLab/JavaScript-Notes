c4dc13b83b27bfcf677f688c5b6c3e0e88777a0d
ShivamWiki <shivam_223033@saitm.org>
2024-03-08 03:32:03 +0530

    Source Code

---
+++ Scope.js
@@ -0,0 +1,29 @@
+// var c = 300;
+let a = 300;
+if (true) {
+  let a = 20;
+  const b = 30;
+
+  console.log(a);
+  // 20
+  //   var v = 50;
+  //   console.log(v);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(v);
+// Output: 50
+
+console.log(a);
+// 300
+/*
+20
+30
+50
+*/
+
+// for (let i = 0; i < array.length; i++) {
+//   const element = array[i];
+// }
+
