cde76aa2bcd40e968b76b54cd2e8626e4dd6e607
ShivamWiki <shivam_223033@saitm.org>
2024-03-08 03:17:17 +0530

    Source Code

--- 20 Functions with objects and array in javascript/FunctionSec.js
+++ 20 Functions with objects and array in javascript/FunctionSec.js
@@ -16,3 +16,33 @@ console.log(calculateCartPrice(20, 40, 60, 80, 100, 120));
 // [ 60, 80, 100, 120 ]
 // 20 and 40 are assigned to val1 and val2 and rest of the values are assigned to num1
 
+const user = {
+  userName: "Shivam",
+  prices: 199,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `User Name is ${anyobject.userName} and price is ${anyobject.prices}`
+  );
+}
+
+// handleObject(user);
+// User Name is Shivam and price is 199
+
+handleObject({
+  userName: "Shivam",
+  prices: 133,
+});
+// User Name is Shivam and price is 133
+
+const myNewArr = [20, 40, 60, 80, 100];
+
+function returnSecondValue(getArr) {
+  return getArr[1];
+}
+
+console.log(returnSecondValue(myNewArr));
+// 40
+console.log(returnSecondValue([1, 2, 5, 8]));
+// 2
