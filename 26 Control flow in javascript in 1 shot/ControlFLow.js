3bcd3118fedb53b4ebfa4ca7300698164ce4b3ce
ShivamWiki <shivam_223033@saitm.org>
2024-03-19 01:14:41 +0530

    Source Code

--- 26 Control flow in javascript in 1 shot/ControlFLow.js
+++ 26 Control flow in javascript in 1 shot/ControlFLow.js
@@ -39,3 +39,34 @@ if (score > 100) {
 // User Power : fly
 // error - power is not defined
 // console.log(`User Power : ${power}`);
+
+const balance = 1000;
+// if (balance > 500) console.log("test");
+
+// Best Way
+// if (balance > 500) {
+//   console.log("test");
+// }
+
+if (balance < 500) {
+  console.log("less than");
+} else if (balance < 750) {
+  console.log("less than 750");
+} else if (balance < 900) {
+  console.log("less than 900");
+} else {
+  console.log("Less than 1200");
+}
+
+const userLoggedIn = true;
+const debitCard = true;
+const logggedInFromGoogle = false;
+const logggedInFromEmail = true;
+
+if (userLoggedIn && debitCard) {
+  console.log("Allow to buy courses");
+}
+
+if (logggedInFromGoogle || logggedInFromEmail) {
+  console.log("User Logged In");
+}
---
+++ 26 Control flow in javascript in 1 shot/TruthyFalsy.js
@@ -0,0 +1,20 @@
+// const userEmail = "Sam@gmail.com";
+// const userEmail = "";
+const userEmail = [];
+
+if (userEmail) {
+  console.log("Got user email");
+} else {
+  console.log("dont have user email");
+}
+
+// falsy values
+
+// false, 0, -0;,BigInt 0n,"",
+// , null ,undefined, NaN
+
+// Truthy values
+
+"0", "false", " ", [], {}, function () {};
+
+// 37:27
