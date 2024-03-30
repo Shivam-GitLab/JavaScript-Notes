4a10f302cfef286249f964dd7675c5a089889b31
ShivamWiki <shivam_223033@saitm.org>
2024-03-31 00:48:18 +0530

    Source Code

---
+++ 27 For loop with break and continue in javascript/Loops.js
@@ -0,0 +1,8 @@
+// for
+
+let array = [10, 20, 30, 40, 50, 60];
+for (let i = 0; i < array.length; i++) {
+  //   const element = array[i];
+  //   console.log(element);
+  console.log(array[i]);
+}
